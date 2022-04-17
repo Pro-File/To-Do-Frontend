86f4771bc93cd3af7f4e64bef138ad2117a9ea33
Muhammad Arbab Hussain Siddiqui <muhammad.arbab.hussain.siddiqui@gmail.com>
2022-04-17 22:27:39 +0500

    Done with isActive implementation for parent and child tasks both!

--- src/Components/Accordions.js
+++ src/Components/Accordions.js
@@ -1,78 +1,83 @@
-import React,{useEffect} from 'react';
-import { styled } from '@mui/material/styles';
-import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
-import MuiAccordion from '@mui/material/Accordion';
-import MuiAccordionSummary from '@mui/material/AccordionSummary';
-import MuiAccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import { useDispatch, useSelector } from 'react-redux';
-import SubList from './SubList';
-import { Checkbox } from '@mui/material';
-import { UpdateStatusOfList } from '../Redux/ToDo/ToDoActions';
-import ToDoServices from '../Services/ToDoServices';
-import SubToDoServices from '../Services/SubToDoServices';
-import { SetSubToDoStatus } from '../Redux/SubToDo/SubToDoActions';
+import React, { useEffect } from "react";
+import { styled } from "@mui/material/styles";
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import MuiAccordion from "@mui/material/Accordion";
+import MuiAccordionSummary from "@mui/material/AccordionSummary";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import { useDispatch, useSelector } from "react-redux";
+import SubList from "./SubList";
+import { Checkbox } from "@mui/material";
+import { UpdateStatusOfList } from "../Redux/ToDo/ToDoActions";
+import ToDoServices from "../Services/ToDoServices";
+import SubToDoServices from "../Services/SubToDoServices";
+import { SetSubToDoStatus } from "../Redux/SubToDo/SubToDoActions";
 
 const Accordion = styled((props) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
 ))(({ theme }) => ({
   border: `1px solid ${theme.palette.divider}`,
-  '&:not(:last-child)': {
+  "&:not(:last-child)": {
     borderBottom: 0,
   },
-  '&:before': {
-    display: 'none',
+  "&:before": {
+    display: "none",
   },
 }));
 
 const AccordionSummary = styled((props) => (
   <MuiAccordionSummary
-    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
     {...props}
   />
 ))(({ theme }) => ({
   backgroundColor:
-    theme.palette.mode === 'dark'
-      ? 'rgba(255, 255, 255, .05)'
-      : 'rgba(0, 0, 0, .03)',
-  flexDirection: 'row-reverse',
-  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
-    transform: 'rotate(90deg)',
+    theme.palette.mode === "dark"
+      ? "rgba(255, 255, 255, .05)"
+      : "rgba(0, 0, 0, .03)",
+  flexDirection: "row-reverse",
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
   },
-  '& .MuiAccordionSummary-content': {
+  "& .MuiAccordionSummary-content": {
     marginLeft: theme.spacing(1),
   },
 }));
 
 const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
   padding: theme.spacing(2),
-  borderTop: '1px solid rgba(0, 0, 0, .125)',
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
 }));
 
-export default function Accordions({getData}) {
-  const [expanded, setExpanded] = React.useState('panel1');
+export default function Accordions({ getData }) {
+  const [expanded, setExpanded] = React.useState("panel1");
   const todos = useSelector((state) => state.toDos);
   const subtodos = useSelector((state) => state.subToDos);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    console.log("Reload!")
-  }, [todos, subtodos])
-  
+    console.log("Reload!");
+  }, [todos, subtodos]);
 
   const handleChange = (panel) => (event, newExpanded) => {
     setExpanded(newExpanded ? panel : false);
   };
 
-
-  
-  const handleToggle = (value) => async() => {
-    const res = await ToDoServices.updateToDo({status: !value.status}, value._id);
-    if(res.data){
-      const filteredChildren = subtodos.filter((item) => item.todo_id === value._id);
-      for(var child of filteredChildren){
-        const result = await SubToDoServices.updateSubToDo({status: res.data.updatedToDo.status}, child._id);
-        if(result.data){
+  const handleToggle = (value) => async () => {
+    const res = await ToDoServices.updateToDo(
+      { status: !value.status },
+      value._id
+    );
+    if (res.data) {
+      const filteredChildren = subtodos.filter(
+        (item) => item.todo_id === value._id
+      );
+      for (var child of filteredChildren) {
+        const result = await SubToDoServices.updateSubToDo(
+          { status: res.data.updatedToDo.status },
+          child._id
+        );
+        if (result.data) {
           dispatch(SetSubToDoStatus(result.data.updatedSubToDo));
         }
       }
@@ -82,29 +87,51 @@ export default function Accordions({getData}) {
 
   return (
     <div>
-     {
-       todos.map((item) => {
+      {todos.map((item) => {
         const labelId = `checkbox-list-secondary-label-${item}`;
-         return <Accordion key={item._id} expanded={expanded === item._id} onChange={handleChange(item._id)}>
-         <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-          <div style={{width: '100%', display:'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-          <Typography>{item.title}</Typography>
-          <Checkbox
-                edge="end"
-                onChange={handleToggle(item)}
-                checked={item.status}
-                inputProps={{ 'aria-labelledby': labelId }}
-              />
-          </div>
-
-         </AccordionSummary>
-         <AccordionDetails >
-          <SubList id={item._id} subtodos={subtodos}/>
-         </AccordionDetails>
-       </Accordion>
-       })
-     }
-
+        return (
+          <Accordion
+            key={item._id}
+            expanded={expanded === item._id}
+            onChange={handleChange(item._id)}
+          >
+            <AccordionSummary
+              aria-controls="panel1d-content"
+              id="panel1d-header"
+            >
+              <div className="main-row">
+                <Typography className="row-text" style={{ width: "70%" }}>
+                  {item.title}
+                </Typography>
+                <Typography className="row-sidearea" style={{ width: "15%" }}>
+                  {
+                    subtodos.filter(
+                      (subtodo) =>
+                        subtodo.todo_id === item._id && subtodo.status === true
+                    ).length
+                  }
+                  <span className="text">of</span>
+                  {
+                    subtodos.filter((subtodo) => subtodo.todo_id === item._id)
+                      .length
+                  }
+                </Typography>
+                <Checkbox
+                  className="row-sidearea"
+                  style={{ width: "15%" }}
+                  edge="end"
+                  onChange={handleToggle(item)}
+                  checked={item.status}
+                  inputProps={{ "aria-labelledby": labelId }}
+                />
+              </div>
+            </AccordionSummary>
+            <AccordionDetails>
+              <SubList id={item._id} subtodos={subtodos} />
+            </AccordionDetails>
+          </Accordion>
+        );
+      })}
     </div>
   );
 }
--- src/Components/SubList.js
+++ src/Components/SubList.js
@@ -1,35 +1,57 @@
-import { useEffect,useState, React } from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import Checkbox from '@mui/material/Checkbox';
-import CreateSubTask from './CreateSubTask';
-import SubToDoServices from '../Services/SubToDoServices';
-import { SetSubToDoStatus } from '../Redux/SubToDo/SubToDoActions';
-import { useDispatch } from 'react-redux';
+import { useEffect, useState, React } from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Checkbox from "@mui/material/Checkbox";
+import CreateSubTask from "./CreateSubTask";
+import SubToDoServices from "../Services/SubToDoServices";
+import { SetSubToDoStatus } from "../Redux/SubToDo/SubToDoActions";
+import { useDispatch } from "react-redux";
+import ToDoServices from "../Services/ToDoServices";
+import { UpdateStatusOfList } from "../Redux/ToDo/ToDoActions";
 
-export default function SubList({id, subtodos}) {
+export default function SubList({ id, subtodos }) {
   const dispatch = useDispatch();
   const [filteredSubToDos, setFilteredSubToDos] = useState([]);
-    useEffect(() => {
-        getFilteredSubToDos();
-    }, [subtodos])
+  useEffect(() => {
+    getFilteredSubToDos();
+  }, [subtodos]);
 
   const getFilteredSubToDos = () => {
     const res = subtodos.filter((item) => item.todo_id === id);
     setFilteredSubToDos([...res]);
-  }
+  };
 
-  const handleToggle = (value) => async() => {
-    const res = await SubToDoServices.updateSubToDo({status: !value.status}, value._id);
-    if(res.data){
+  const handleToggle = (value) => async () => {
+    let count = value.status ? 1 : -1;
+    const subFilteredChildren = subtodos.filter((item) => item.todo_id === id);
+    subFilteredChildren.forEach((item) => {
+      if (item.status === false) {
+        count++;
+      }
+    });
+    if (count > 0) {
+      const res = await ToDoServices.updateToDo({ status: false }, id);
+      dispatch(UpdateStatusOfList(res.data.updatedToDo));
+    } else {
+      const res = await ToDoServices.updateToDo({ status: true }, id);
+      dispatch(UpdateStatusOfList(res.data.updatedToDo));
+    }
+    const res = await SubToDoServices.updateSubToDo(
+      { status: !value.status },
+      value._id
+    );
+    if (res.data) {
       dispatch(SetSubToDoStatus(res.data.updatedSubToDo));
     }
   };
 
   return (
-    <List dense sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+    <List
+      dense
+      sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+    >
       {filteredSubToDos.map((item) => {
         const labelId = `checkbox-list-secondary-label-${item}`;
         return (
@@ -40,7 +62,7 @@ export default function SubList({id, subtodos}) {
                 edge="end"
                 onChange={handleToggle(item)}
                 checked={item.status}
-                inputProps={{ 'aria-labelledby': labelId }}
+                inputProps={{ "aria-labelledby": labelId }}
               />
             }
             disablePadding
@@ -51,7 +73,7 @@ export default function SubList({id, subtodos}) {
           </ListItem>
         );
       })}
-      <CreateSubTask parentID = {id}/>
+      <CreateSubTask parentID={id} />
     </List>
   );
 }
